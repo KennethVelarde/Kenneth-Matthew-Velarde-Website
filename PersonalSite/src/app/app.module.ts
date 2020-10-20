c565554bcce7fdd771f9b6245177aeaf15139a1a
Kenneth-Matthew Velarde <velardekenneth@gmail.com>
2020-10-19 21:51:19 -0700

    added about, home, sidebar, work components

--- PersonalSite/src/app/app.module.ts
+++ PersonalSite/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    WorkComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
