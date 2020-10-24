95ef1441cf2692393dc4a59bff2924390784686d
Kenneth-Matthew Velarde <velardekenneth@gmail.com>
2020-10-24 10:33:59 -0700

    nav bar structure now set, nav bar now fixed when routing

--- PersonalSite/src/app/app.module.ts
+++ PersonalSite/src/app/app.module.ts
@@ -2,19 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
 import { WorkComponent } from './work/work.component';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
     HomeComponent,
     WorkComponent,
-    AboutComponent
+    AboutComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
