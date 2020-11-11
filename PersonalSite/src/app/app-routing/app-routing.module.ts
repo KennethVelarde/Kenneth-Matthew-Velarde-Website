a3b0f95add0b2bc8086dcdc1dfda32c86602d51b
Kenneth-Matthew Velarde <velardekenneth@gmail.com>
2020-11-10 16:41:25 -0800

    children route restructuring

--- PersonalSite/src/app/app-routing/app-routing.module.ts
+++ PersonalSite/src/app/app-routing/app-routing.module.ts
@@ -6,9 +6,11 @@ import { AboutComponent } from '../about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'work', component: WorkComponent },
+  { path: 'home', component: HomeComponent, children: [
+    { path: 'about', component: AboutComponent },
+    { path: 'work', component: WorkComponent }
+    // { path: 'about', component: AboutComponent },
+  ] },
   { path: '**', component: HomeComponent }
 ];
 
