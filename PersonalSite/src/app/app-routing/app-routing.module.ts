f5c189558f15b1b47704ec03c2a56be5f8de1729
Kenneth-Matthew Velarde <velardekenneth@gmail.com>
2020-11-12 21:01:03 -0800

    nav to vertical nav

--- PersonalSite/src/app/app-routing/app-routing.module.ts
+++ PersonalSite/src/app/app-routing/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   ] },
   // { path: 'home', component: HomeComponent},
   // {path: 'about', component:AboutComponent},
-  { path: '**', redirectTo:"/about", pathMatch:'full' }
+  { path: '**', redirectTo:"about", pathMatch:'full' }
 ];
 
 @NgModule({
